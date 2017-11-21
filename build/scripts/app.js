160cbc68c863c158992790874236f5cc1b29157e
averyethomas <averyethomas@gmail.com>
2017-11-21 17:01:09 -0500

    attempt to get followed_by list but not accessible via api anymore

--- build/scripts/app.js
+++ build/scripts/app.js
@@ -4,25 +4,44 @@ app.controller('instaCtrl', ['$scope', '$http', function ($scope, $http) {
     
     $scope.username = 'averyethomas';
     $scope.accessToken = '42820715.160a976.c5a1c2ea539141879bb411e8ec520ec3';
-    $scope.url = 'https://api.instagram.com/v1/users/self/media/recent/?access_token=' + $scope.accessToken + '&count=-1';
-    $scope.instaData = [];
 
-    $scope.getData = function(){
+
+    $scope.PostsUrl = 'https://api.instagram.com/v1/users/self/media/recent/?access_token=' + $scope.accessToken;
+    $scope.posts = [];
+
+    $scope.getPosts = function(){
         $http({
             method: 'GET',
-            url: $scope.url,
+            url: $scope.PostsUrl,
         }).
             success(function(data, status, headers, config){
-                $scope.currentDataNumber = data.length;
-                $scope.instaData = data.data;
-                console.log('success');
-                console.log($scope.instaData);
+                $scope.posts = data.data;
+                console.log($scope.posts);
         }).
             error(function(data, status, headers, config){
                 console.log('error');
             });
     };
     
-    $scope.getData();
+    $scope.getPosts();
+
+    $scope.postID = '1652432592497508217';
+    $scope.likesUrl = 'https://api.instagram.com/v1/media/' + $scope.postID + '/likes?access_token=' + $scope.accessToken;
+
+    $scope.getLikes = function(){
+        $http({
+            method: 'GET',
+            url: $scope.likesUrl,
+        }).
+        success(function(data, status, headers, config){
+            $scope.likes = data.data;
+            console.log($scope.likes)
+        }).
+        error(function(data, status, headers, config){
+            console.log('error');
+        });
+    };
+
+    $scope.getLikes();
     
 }]);
